040baff3708cd15eea2ac4b9bd308a9d7a39683c
gsmolinski <g.smolinski1@gmail.com>
2021-11-06 17:44:07 +0100

    add module to choose from existing projects

---
+++ R/choose_existing_project_mod.R
@@ -0,0 +1,15 @@
+choose_existing_project_ui <- function(id) {
+  
+  ns <- NS(id)
+  
+  tagList(
+    selectizeInput(ns("choose_project"), "Or choose existing project", choices = NULL,
+                   options = list(onInitialize = I('function() { this.setValue(""); }')))
+  )
+}
+
+choose_existing_project_server <- function(id, db_con) {
+  moduleServer(id, function(input, output, session) {
+    updateSelectizeInput(session = session, "choose_project", choices = dbGetQuery(db_con, "SELECT name FROM projects ORDER BY name"))
+  })
+}
