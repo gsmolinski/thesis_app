759185190cf476a8170f1e2bd0e7e0c56a3c586a
gsmolinski <g.smolinski1@gmail.com>
2021-11-06 17:45:07 +0100

    add database connection and module for choosing projects

--- app.R
+++ app.R
@@ -1,18 +1,25 @@
 library(shiny)
 library(shinyalert)
+library(DBI)
+
 ui <- fluidPage(
   useShinyalert(),
   titlePanel("Code OE"),
   tabsetPanel(
     tabPanel(title = "Import",
              br(),
-             import_file_ui("import")
+             fluidRow(
+               column(3, import_file_ui("import")),
+               column(3, offset = 2, choose_existing_project_ui("existing_project"))
+             )
     )
   )
 )
 
 server <- function(input, output, session) {
+  db_con <- dbConnect(RSQLite::SQLite(), "data/oe_database.db")
   imported_file <- import_file_server("import")
+  choose_existing_project_server("existing_project", db_con)
 }
 
 shinyApp(ui, server)
