ebd20052fb9bb1b785841a4bad27957e7ed9fcd4
gsmolinski <g.smolinski1@gmail.com>
2021-11-07 17:23:22 +0100

    remove req because we don't want to stop computation, use isTruthy instead

--- R/import_file_mod.R
+++ R/import_file_mod.R
@@ -4,24 +4,30 @@ import_file_ui <- function(id) {
   
   tagList(
     fileInput(ns("imported_file"), "Choose new file (project)",
-              accept = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+              accept = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+    
   )
 }
 
 import_file_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     
-    reactive({
+    load_file <- reactive({
       file <- input$imported_file
-      req(file)
-     file <- readxl::read_xlsx(file$datapath, guess_max = 1000000)
-     
-     if (!all(names(file) == c("id", "variable", "value")) || ncol(file) != 3) {
-       shinyalert(text = "File must contain only three columns: id, variable, value.")
-     } else {
-       file
-     }
+      if (isTruthy(file)) {
+        file_data <- readxl::read_xlsx(file$datapath, guess_max = 1000000)
+        
+        if (!all(names(file_data) == c("id", "variable", "value")) || ncol(file_data) != 3) {
+          FALSE
+        } else {
+          list(file = file_data, file_name = stri_sub(file$name, 1, -6))
+        }
+      } else {
+        FALSE
+      }
     })
+    
+    return(list(load_file = load_file, file_input = reactive(input$imported_file)))
     
   })
 }
