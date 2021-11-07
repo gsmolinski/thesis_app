dd2e44ff9028282f12fca8a9f50e4e32de14719e
gsmolinski <g.smolinski1@gmail.com>
2021-11-07 17:24:32 +0100

    add modules and debug actionButton

--- app.R
+++ app.R
@@ -1,25 +1,47 @@
 library(shiny)
 library(shinyalert)
 library(DBI)
+library(stringi)
+library(dplyr)
 
 ui <- fluidPage(
   useShinyalert(),
   titlePanel("Code OE"),
+  actionButton("debug", "Debug"), # remove later
   tabsetPanel(
     tabPanel(title = "Import",
              br(),
              fluidRow(
                column(3, import_file_ui("import")),
-               column(3, offset = 2, choose_existing_project_ui("existing_project"))
+               column(3, offset = 1, choose_existing_project_ui("existing_project"))
+             ),
+             br(),
+             fluidRow(
+               column(2, load_project_ui("load"))
              )
-    )
+    ),
+    tabPanel(title = "Code",
+             br()
+             )
   )
 )
 
 server <- function(input, output, session) {
   db_con <- dbConnect(RSQLite::SQLite(), "data/oe_database.db")
-  imported_file <- import_file_server("import")
-  choose_existing_project_server("existing_project", db_con)
+  
+  imported <- import_file_server("import")
+  chosen_project <- choose_existing_project_server("existing_project", db_con)
+  loaded_project <- load_project_server("load", db_con,
+                      imported = imported,
+                      chosen_project = chosen_project)
+  
+  observeEvent(input$debug, {
+    browser()
+  }) # remove later
+  
+  onStop(function() {
+    dbDisconnect(db_con)
+  })
 }
 
-shinyApp(ui, server)
+runApp(shinyApp(ui, server))
